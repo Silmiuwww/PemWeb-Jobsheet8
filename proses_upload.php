6abcbfbcc0165f999461c6c8c9e204de443a26e1
Silmiuww <nadiasalma078@gmail.com>
2025-10-22 23:07:19 +0700

    Praktikum 2 soal no 9

--- proses_upload.php
+++ proses_upload.php
@@ -1,23 +1,46 @@
 <?php
-$targetDirectory = "documents/"; 
-
+$targetDirectory = "images/"; 
 if (!file_exists($targetDirectory)) {
-    mkdir($targetDirectory, 0777, true);
+    if (!mkdir($targetDirectory, 0777, true)) {
+        die("Gagal membuat direktori unggahan.");
+    }
 }
 
-if ($_FILES['files']['name'][0]) {
+$allowedExtensions = array("jpg", "jpeg", "png", "gif");
+$maxsize = 5 * 1024 * 1024; 
+
+if (isset($_FILES['files']['name'][0]) && !empty($_FILES['files']['name'][0])) {
     $totalFiles = count($_FILES['files']['name']);
+    
+    echo "<h3>Hasil Multi Upload Gambar:</h3>";
 
     for ($i = 0; $i < $totalFiles; $i++) {
         $fileName = $_FILES['files']['name'][$i];
-        $targetFile = $targetDirectory . $fileName;
+        $fileSize = $_FILES['files']['size'][$i];
+        $fileTmp = $_FILES['files']['tmp_name'][$i];
+        
+        $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
 
-        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
-            echo "File $fileName berhasil diunggah.<br>";
+        $targetFile = $targetDirectory . $fileName;
+        
+        if (!in_array($fileType, $allowedExtensions)) {
+            echo "File **$fileName** gagal: Ekstensi tidak diizinkan ($fileType). Hanya (JPG, PNG, GIF).<br>";
+            continue; 
+        }
+
+        if ($fileSize > $maxsize) {
+            echo "File **$fileName** gagal: Ukuran file melebihi batas 5 MB.<br>";
+            continue; 
+        }
+        
+       
+        if (move_uploaded_file($fileTmp, $targetFile)) {
+            echo "File **$fileName** berhasil diunggah. <br>";
         } else {
-            echo "Gagal mengunggah file $fileName.<br>";
+            echo "Gagal mengunggah file **$fileName**. (Kesalahan server/izin).<br>";
         }
     }
+    
 } else {
     echo "Tidak ada file yang diunggah.";
 }
